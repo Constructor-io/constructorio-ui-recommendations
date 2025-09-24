9f63297602d90f457a26a6b67181a16194eba2bc
Evan Yan <evan.yan@constructor.io>
2025-09-24 12:25:52 -0700

    Update hook and tests

---
+++ spec/hooks/useCioClient/useCioClient.server.test.js
@@ -0,0 +1,39 @@
+import useCioClient from '../../../src/hooks/useCioClient';
+import { renderHookServerSide } from '../../test-utils.server';
+
+describe('Hook on Server: useCioClient', () => {
+  beforeEach(() => {
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should throw error if Api Key not provided', () => {
+    expect(() =>
+      renderHookServerSide(() => useCioClient(), {
+        initialProps: {},
+      }),
+    ).toThrow('Api Key or Constructor Client required');
+  });
+
+  it('should return client when custom client is provided', () => {
+    const mockClient = { tracker: () => {} };
+    const { result } = renderHookServerSide(({ cioClient }) => useCioClient({ cioClient }), {
+      initialProps: { cioClient: mockClient },
+    });
+
+    expect(result).toBe(mockClient);
+  });
+
+  it('should return null when it is not called in client environment', () => {
+    const key = 'xx';
+    const { result } = renderHookServerSide(({ apiKey }) => useCioClient({ apiKey }), {
+      initialProps: { apiKey: key },
+    });
+
+    expect(result).toBe(null);
+  });
+});
---
+++ src/hooks/useCioClient.ts
@@ -0,0 +1,38 @@
+import ConstructorIOClient from '@constructor-io/constructorio-client-javascript';
+import {
+  ConstructorClientOptions,
+  Nullable,
+} from '@constructor-io/constructorio-client-javascript/lib/types';
+import { useMemo } from 'react';
+import version from '../version';
+
+type UseCioClientProps = {
+  apiKey?: string;
+  cioClient?: Nullable<ConstructorIOClient>;
+  options?: Omit<ConstructorClientOptions, 'apiKey' | 'sendTrackingEvents' | 'version'>;
+};
+
+type UseCioClient = (props: UseCioClientProps) => Nullable<ConstructorIOClient> | never;
+
+const useCioClient: UseCioClient = ({ apiKey, cioClient, options } = {}) => {
+  if (!apiKey && !cioClient) {
+    throw new Error('Api Key or Constructor Client required');
+  }
+
+  const memoizedCioClient = useMemo(() => {
+    if (cioClient) return cioClient;
+    if (apiKey && typeof window !== 'undefined') {
+      return new ConstructorIOClient({
+        apiKey,
+        sendTrackingEvents: true,
+        version: `cio-ui-recommendations-${version}`,
+        ...options,
+      });
+    }
+
+    return null;
+  }, [apiKey, cioClient, options]);
+  return memoizedCioClient!;
+};
+
+export default useCioClient;
