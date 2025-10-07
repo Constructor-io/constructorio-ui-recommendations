caad5ac3403e9a9d4a0bbf55faea51dbd364ace5
Evan Yan <evan.yan@constructor.io>
2025-10-07 13:42:41 -0700

    Update test variable naming

--- spec/utils/transformers.test.ts
+++ spec/utils/transformers.test.ts
@@ -15,11 +15,11 @@ const testPodData = {
 
 describe('Testing Transformers: transformPodData', () => {
   it('should transform pod data with basic properties', () => {
-    const transformedPod = transformPodData(testPodData);
+    const result = transformPodData(testPodData);
 
-    expect(transformedPod.id).toBe(testPodData.id);
-    expect(transformedPod.displayName).toBe(testPodData.display_name);
-    expect(transformedPod.channels).toEqual(testPodData.channels);
+    expect(result.id).toBe(testPodData.id);
+    expect(result.displayName).toBe(testPodData.display_name);
+    expect(result.channels).toEqual(testPodData.channels);
   });
 
   it('should include additional properties from the input object', () => {
@@ -28,77 +28,77 @@ describe('Testing Transformers: transformPodData', () => {
       extraField: 'extraValue',
     };
 
-    const transformedPod = transformPodData(testPodDataWithExtraField);
+    const result = transformPodData(testPodDataWithExtraField);
 
-    expect(transformedPod.extraField).toBe(testPodDataWithExtraField.extraField);
+    expect(result.extraField).toBe(testPodDataWithExtraField.extraField);
   });
 });
 
 describe('Testing Transformers: transformResultVariation', () => {
   it('should transform item variation with basic properties', () => {
     const testVariant = testItem.variations[0];
-    const transformedVariation = transformResultVariation(testVariant);
+    const result = transformResultVariation(testVariant);
 
     // Transformed base properties
-    expect(typeof transformedVariation.itemName).toBe('string');
-    expect(transformedVariation.itemName).toBe(testVariant.value);
+    expect(typeof result.itemName).toBe('string');
+    expect(result.itemName).toBe(testVariant.value);
 
-    expect(typeof transformedVariation.variationId).toBe('string');
-    expect(transformedVariation.variationId).toBe(testVariant.data.variation_id);
+    expect(typeof result.variationId).toBe('string');
+    expect(result.variationId).toBe(testVariant.data.variation_id);
 
     // Flattened properties
-    expect(typeof transformedVariation.url).toBe('string');
-    expect(transformedVariation.url).toBe(testVariant.data.url);
+    expect(typeof result.url).toBe('string');
+    expect(result.url).toBe(testVariant.data.url);
 
-    expect(typeof transformedVariation.imageUrl).toBe('string');
-    expect(transformedVariation.imageUrl).toBe(testVariant.data.image_url);
+    expect(typeof result.imageUrl).toBe('string');
+    expect(result.imageUrl).toBe(testVariant.data.image_url);
 
     // Ensure remaining metadata fields are captured in `data`
-    expect(transformedVariation.data.price).toBe(testVariant.data.price);
-    expect(transformedVariation.data.swatchPreview).toBe(testVariant.data.swatchPreview);
-    expect(transformedVariation.data.rolloverImage).toBe(testVariant.data.rolloverImage);
+    expect(result.data.price).toBe(testVariant.data.price);
+    expect(result.data.swatchPreview).toBe(testVariant.data.swatchPreview);
+    expect(result.data.rolloverImage).toBe(testVariant.data.rolloverImage);
   });
 });
 
 describe('Testing Transformers: transformResultItem', () => {
   it('should return all base properties as camelCased properties', () => {
-    const transformedItem = transformResultItem(testItem);
+    const result = transformResultItem(testItem);
 
     // Transformed base properties
-    expect(typeof transformedItem.itemName).toBe('string');
-    expect(transformedItem.itemName).toBe(testItem.value);
+    expect(typeof result.itemName).toBe('string');
+    expect(result.itemName).toBe(testItem.value);
 
-    expect(typeof transformedItem.matchedTerms).toBe('object');
-    expect(transformedItem.matchedTerms).toBe(testItem.matched_terms);
+    expect(typeof result.matchedTerms).toBe('object');
+    expect(result.matchedTerms).toBe(testItem.matched_terms);
 
-    expect(typeof transformedItem.isSlotted).toBe('boolean');
-    expect(transformedItem.isSlotted).toBe(testItem.is_slotted);
+    expect(typeof result.isSlotted).toBe('boolean');
+    expect(result.isSlotted).toBe(testItem.is_slotted);
 
-    expect(typeof transformedItem.variations).toBe('object');
-    expect(transformedItem.variations?.length).toBe(testItem.variations.length);
+    expect(typeof result.variations).toBe('object');
+    expect(result.variations?.length).toBe(testItem.variations.length);
 
     // Flattened properties
-    expect(typeof transformedItem.itemId).toBe('string');
-    expect(transformedItem.itemId).toBe(testItem.data.id);
+    expect(typeof result.itemId).toBe('string');
+    expect(result.itemId).toBe(testItem.data.id);
 
-    expect(typeof transformedItem.variationId).toBe('string');
-    expect(transformedItem.variationId).toBe(testItem.data.variation_id);
+    expect(typeof result.variationId).toBe('string');
+    expect(result.variationId).toBe(testItem.data.variation_id);
 
-    expect(typeof transformedItem.url).toBe('string');
-    expect(transformedItem.url).toBe(testItem.data.url);
+    expect(typeof result.url).toBe('string');
+    expect(result.url).toBe(testItem.data.url);
 
-    expect(typeof transformedItem.imageUrl).toBe('string');
-    expect(transformedItem.imageUrl).toBe(testItem.data.image_url);
+    expect(typeof result.imageUrl).toBe('string');
+    expect(result.imageUrl).toBe(testItem.data.image_url);
 
-    expect(typeof transformedItem.description).toBe('string');
-    expect(transformedItem.description).toBe(testItem.data.description);
+    expect(typeof result.description).toBe('string');
+    expect(result.description).toBe(testItem.data.description);
 
-    expect(typeof transformedItem.groupIds).toBe('object');
-    expect(transformedItem.groupIds).toBe(testItem.data.group_ids);
+    expect(typeof result.groupIds).toBe('object');
+    expect(result.groupIds).toBe(testItem.data.group_ids);
 
     // Ensure remaining metadata fields are captured in `data`
-    expect(transformedItem.data.price).toBe(testItem.data.price);
-    expect(transformedItem.data.altPrice).toBe(testItem.data.altPrice);
+    expect(result.data.price).toBe(testItem.data.price);
+    expect(result.data.altPrice).toBe(testItem.data.altPrice);
   });
 });
 
