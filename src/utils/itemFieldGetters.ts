b621bbb96776869503550292d5285275855e77d1
Evan Yan <evan.yan@constructor.io>
2025-10-09 12:09:17 -0700

    Add itemFieldGetters types and default implementation

---
+++ src/utils/itemFieldGetters.ts
@@ -0,0 +1,45 @@
+import { ItemFieldGetters, Item, SwatchItem, Variation } from '../types';
+
+export function getPrice(item: Item | Variation): number {
+  return item.data.price;
+}
+
+export function getSalePrice(item: Item | Variation): number | undefined {
+  return item.data.salePrice;
+}
+
+export function getRolloverImage(item: Item | Variation): string | undefined {
+  return item.data.rolloverImage;
+}
+
+export function getSwatchPreview(variation: Variation): string | undefined {
+  return variation?.data?.swatchPreview;
+}
+
+/* eslint-disable-next-line max-params */
+export function getSwatches(
+  item: Item,
+  retrievePrice: ItemFieldGetters['getPrice'],
+  retrieveSalePrice: ItemFieldGetters['getSalePrice'],
+  retrieveRolloverImage: ItemFieldGetters['getRolloverImage'],
+  retrieveSwatchPreview: ItemFieldGetters['getSwatchPreview'],
+): SwatchItem[] | undefined {
+  const swatchList: SwatchItem[] = [];
+
+  item?.variations?.forEach((variation: Variation) => {
+    if (retrieveSwatchPreview(variation)) {
+      swatchList.push({
+        itemName: variation?.itemName || item?.itemName,
+        url: variation?.url || item?.url,
+        imageUrl: variation?.url,
+        variationId: variation?.variationId,
+        price: retrievePrice(variation),
+        salePrice: retrieveSalePrice(variation),
+        swatchPreview: retrieveSwatchPreview(variation),
+        rolloverImage: retrieveRolloverImage(variation),
+      });
+    }
+  });
+
+  return swatchList;
+}
