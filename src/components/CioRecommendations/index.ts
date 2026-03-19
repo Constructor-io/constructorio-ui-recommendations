965dcb9d3d466af164a9756619a961583bbe7a67
mudaafi <ahmad.mudaafi@constructor.io>
2026-03-20 02:53:12 +0300

    update exports

--- src/components/CioRecommendations/index.ts
+++ src/components/CioRecommendations/index.ts
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-restricted-exports
 export { default } from './CioRecommendations';
 
-export * from './CioRecommendations';
+export type {
+  CioRecommendationsRenderProps,
+  CioRecommendationsComponentOverrides,
+  CioRecommendationsProps,
+} from './CioRecommendations';
+
 export * from './CioRecommendationsError';
 export * from './CioRecommendationsLoading';
 export { default as CioRecommendationsProvider } from './CioRecommendationsProvider';
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,51 @@ import CioRecommendations from './components/CioRecommendations';
 
 // Components
 export default CioRecommendations;
-export * from './components/CioRecommendations';
-export * from './components/PodHeader';
+export {
+  default as CioRecommendations,
+  CioRecommendationsProvider,
+} from './components/CioRecommendations';
+export { default as PodHeader } from './components/PodHeader';
 
 // Hooks
+export { useCioRecommendationContext } from './hooks/useCioRecommendationContext';
 export { default as useCioClient } from './hooks/useCioClient';
-export { default as useRecommendationResults } from './hooks/useRecommendationResults';
+export {
+  default as useRecommendationResults,
+  isResponseLoaded,
+  isResponseLoading,
+  isResponseError,
+} from './hooks/useRecommendationResults';
 
 // Utils
 export * as utils from './utils';
 
 // Types
 export * from './types';
-export {
-  CIO_EVENTS,
+export { CIO_EVENTS } from '@constructor-io/constructorio-ui-components';
+export type {
   ProductCardEventDetail,
   CarouselNavEventDetail,
   CioEventDetailMap,
 } from '@constructor-io/constructorio-ui-components';
+export type {
+  CioRecommendationsProps,
+  CioRecommendationsRenderProps,
+  CioRecommendationsComponentOverrides,
+} from './components/CioRecommendations';
+export type {
+  CioRecommendationsErrorOverrides,
+  CioRecommendationsErrorProps,
+} from './components/CioRecommendations';
+export type {
+  CioRecommendationsLoadingOverrides,
+  CioRecommendationsLoadingProps,
+} from './components/CioRecommendations';
+export type { PodHeaderOverrides, PodHeaderProps } from './components/PodHeader';
+export type {
+  UseRecommendationResultsProps,
+  UseRecommendationResultsReturn,
+  UseRecommendationsResultsReturnSuccess,
+  UseRecommendationsResultsReturnFailure,
+  UseRecommendationsResultsReturnLoading,
+} from './hooks/useRecommendationResults';
