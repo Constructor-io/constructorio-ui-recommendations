5f6b93486bffacda045c1f224cb0d29c55bdef9a
Ahmad Mudaafi <mudaafi@gmail.com>
2026-03-20 22:09:40 +0300

    [CDX-300] Recommendations UI Pre-Launch Updates (#30)
    * update exports
    
    * update license-checker
    
    * lint
    
    * update codeowners
    
    * Update .github/CODEOWNERS
    
    Co-authored-by: Enes Kutay SEZEN <eneskutaysezen@gmail.com>
    
    * update workflow
    
    * add bundled.jsx
    
    * fix compile process
    
    * add exclusion to license
    
    ---------
    
    Co-authored-by: Enes Kutay SEZEN <eneskutaysezen@gmail.com>

---
+++ src/bundled.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CioRecommendationsComponent from './components/CioRecommendations';
+import versionNumber from './version';
+import './styles.css';
+
+const CioRecommendations = ({ selector, includeCSS = true, ...rest }) => {
+  if (document) {
+    const stylesheet = document.getElementById('cio-recommendations-styles');
+    const containerSelector = selector;
+    const containerElement = containerSelector ? document.querySelector(containerSelector) : null;
+
+    if (!containerElement) {
+      // eslint-disable-next-line no-console
+      console.error(`CioRecommendations: There were no elements found for the provided selector`);
+
+      return;
+    }
+
+    if (stylesheet) {
+      if (!includeCSS) {
+        stylesheet.disabled = true;
+      } else {
+        stylesheet.disabled = false;
+      }
+    }
+
+    ReactDOM.createRoot(containerElement).render(
+      <React.StrictMode>
+        <CioRecommendationsComponent
+          {...rest}
+          cioClientOptions={{
+            ...rest.cioClientOptions,
+            version: `cio-ui-recommendations-bundled-${versionNumber}`,
+          }}
+        />
+      </React.StrictMode>,
+    );
+  }
+};
+
+if (window) {
+  window.CioRecommendations = CioRecommendations;
+}
+
+export default CioRecommendations;
